f186b7184c28ef23662843039fb06b25eb87f3e5
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-02-13 23:57:36 -0300

    Se agrega modulo api-gateway

--- customer/src/main/java/com/punysis/customer/controllers/CustomerController.java
+++ customer/src/main/java/com/punysis/customer/controllers/CustomerController.java
@@ -2,7 +2,6 @@ package com.punysis.customer.controllers;
 
 import com.punysis.customer.entities.Customer;
 import com.punysis.customer.services.CustomerService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
